136d4cd5b657865bd72f2ab2a8f8279c83a06c2f
SkarY <49595002+SkarYxD@users.noreply.github.com>
2019-10-20 22:05:43 -0300

    ACTUALIZACIÓN
    Actualizado y traducido al español.

--- header.php
+++ header.php
@@ -9,7 +9,9 @@ function banner(){
 |        ___/ / /  / / /___             |
 |       /____/_/  /_/\____/ ~ \033[91mTools     \033[92m|
 |    \033[91mSimple - Multi - Checker           \033[92m|
-|     \033[92m Coded by \033[91mMr.Luciferz             \033[92m|
+|     \033[92m Creado por \033[91mMr.Luciferz           \033[92m|
+|     \033[92m Traducido por \033[91mSkarYxD            \033[92m|
+|     \033[92m Actualizado por \033[91mSkarYxD          \033[92m|
 |        Copyright © $Y               |
 -----------------------------------------\033[97m\n";
 }
